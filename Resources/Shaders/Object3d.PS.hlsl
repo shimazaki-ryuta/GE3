a034946f399d60e3c80fc8eb110dd8d7203890c1
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2023-12-05 10:06:42 +0900

    BlinnphongReflectionModel追加

--- Resources/Shaders/Object3d.PS.hlsl
+++ Resources/Shaders/Object3d.PS.hlsl
@@ -26,7 +26,9 @@ PixelShaderOutput main(VertexShaderOutput input){
 	float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
 	float32_t3 reflectLight = reflect(gDirectionalLight.direction,normalize(input.normal));
 	float RdotE = dot(reflectLight,toEye);
-	float specularPow = pow(saturate(RdotE),gMaterial.shininess);
+	float32_t3 halfVector = normalize(-gDirectionalLight.direction + toEye);
+	float NDotH = dot(normalize(input.normal),halfVector);
+	float specularPow = pow(saturate(NDotH),gMaterial.shininess);
 	float32_t3 diffuse;
 	float32_t3 specular;
 	PixelShaderOutput output;
