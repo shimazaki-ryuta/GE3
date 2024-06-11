d86bd8cf00310ebd86b4f062ba6881b1df0a9c87
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2024-06-11 17:06:22 +0900

    skyboxマージ

--- project/GameScene.h
+++ project/GameScene.h
@@ -31,8 +31,6 @@
 #include "LockOn.h"
 #include "Engine/Animation.h"
 #include "Engine/SkyBox.h"
-#include "Scene/SceneLoader.h"
-#include "GameObject/GameObject.h"
 class GameScene
 {
 public:
@@ -84,11 +82,12 @@ public:
 	void Update();
 	void Draw3D();
 	void Draw2D();
-	
+	void TransitionFade();
+
 
 private:
-	
-	
+
+
 	Microsoft::WRL::ComPtr<ID3D12Resource> pointLightResource;
 	PointLight* pointLightData = nullptr;
 
@@ -100,18 +99,89 @@ private:
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 
-	std::unique_ptr<DebugCamera> debugCamera_;
-	bool isDebugCameraActive_ = false;
 
-	struct Transforms cameraTransform { {1.0f, 1.0f, 1.0f}, { 0.0f,0.0f,0.0f }, { 0.0f,0.0f,-5.0f } };
+	struct Transforms cameraTransform { { 1.0f, 1.0f, 1.0f }, { 0.0f,0.0f,0.0f }, { 0.0f,0.0f,-5.0f } };
 
 	// ビュープロジェクション
 	ViewProjection viewProjection_;
-//1フレーム前の入力情報
+
+	// 自キャラ
+	std::unique_ptr<Player> player_;
+
+	std::unique_ptr<Model> modelPlayerBody_;
+	std::unique_ptr<Model> modelPlayerHead_;
+	std::unique_ptr<Model> modelPlayerL_arm_;
+	std::unique_ptr<Model> modelPlayerR_arm_;
+
+	std::unique_ptr<Model> modelPlayerBody2_;
+	std::unique_ptr<Model> modelPlayerHead2_;
+	std::unique_ptr<Model> modelPlayerL_arm2_;
+	std::unique_ptr<Model> modelPlayerR_arm2_;
+
+	std::unique_ptr<Model> modelWepon_;
+
+
+	std::unique_ptr<Player2> player2_;
+
+	bool isDebugCameraActive_ = false;
+	//DebugCamera* debugCamera_ = nullptr;
+
+	//std::unique_ptr<Skydome> skydome_;
+	//Model* modelSkydome_ = nullptr;
+
+	std::unique_ptr<Ground> ground_;
+	Model* modelGround_ = nullptr;
+
+	std::array<std::unique_ptr<Flooar>, size_t(5)> flooars_;
+	//std::unique_ptr<Flooar> flooar_;
+
+	//std::unique_ptr<Goal> goal_;
+
+	std::unique_ptr<FollowCamera> followCamera_;
+	std::unique_ptr<LockOn> lockOn_;
+
+	Microsoft::WRL::ComPtr<ID3D12Resource> directinalLightResource;
+	DirectionalLight* directinalLightData = nullptr;
+
+	std::unique_ptr<Particle> particle;
+
+	std::unique_ptr<Model> modelBullet_;
+
+	std::unique_ptr<PlayerAI> ai_;
+
+	uint32_t backTextureHandle_;
+	std::unique_ptr<Sprite> backSprite_;
+	bool isEnd_;
+	uint32_t endTextureHandle_[2];
+	std::unique_ptr<Sprite> endSprite_;
+
+	bool isIngame_;
+	//1フレーム前の入力情報
 	XINPUT_STATE preJoyState_;
-	
+	uint32_t pressATextureHandle_;
+	std::unique_ptr<Sprite> pressASprite_;
+	int32_t endCount_;
+	bool isButtonDraw_;
+	int32_t buttonCount_;
+	uint32_t shotTextureHandle_;
+	uint32_t dashTextureHandle_;
+	uint32_t jumpTextureHandle_;
+	std::unique_ptr<Sprite> shotSprite_;
+	std::unique_ptr<Sprite> dashSprite_;
+	std::unique_ptr<Sprite> jumpSprite_;
+	uint32_t titleTextureHandle_;
+	std::unique_ptr<Sprite> titleSprite_;
+
+	uint32_t audioHandle_;
+	int32_t shadeType_ = 0;
+	uint32_t toonShadowTextureHandle_;
+
+	std::unique_ptr<Sprite> fadeSprite_;
+	bool isTransitionFade_;
+	bool isStart_;
+	float fadeAlpha_;
 	D3D12_GPU_DESCRIPTOR_HANDLE srvHandleGPU;
-	uint32_t pointLightMax_=32;
+	uint32_t pointLightMax_ = 32;
 	int colorPhase_;
 	float color_;
 
@@ -120,9 +190,4 @@ private:
 
 	std::unique_ptr<SkyBox> skyBox_;
 	WorldTransform worldTransformSkyBox_;
-
-	std::unique_ptr<SceneLoader> sceneLoader_;
-	std::map<std::string, std::unique_ptr<Model>> modelList_;
-	std::vector<std::unique_ptr<GameObject>> objects_;
 };
-
